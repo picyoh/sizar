a679110acea2161bf24562d874a97e3a0a802cb2
picyoh <picotyoh@gmail.com>
2025-04-21 14:16:36 +0200

    feat: :sparkles: add object detection button
    add object detection button + eventListeners to trigger video process

--- src/components/Stream.tsx
+++ src/components/Stream.tsx
@@ -8,18 +8,23 @@ import { processCV } from "@/lib/processCV";
 
 export default function Stream() {
   //TODO: resizeObserver trigger when resizing window ?
-  // Canvas element
+  // Canvas elements
+  const inputElement = useRef<HTMLCanvasElement>(null);
   const outputElement = useRef<HTMLCanvasElement>(null);
+  // Buttons elements
+  const objButton = useRef<HTMLButtonElement>(null);
+  const [objMode, setObjMode] = useState(false);
   // Video element and its sizes
   const [videoElement, rect] = useResizeObserver();
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   // Video stream from camera
-  const {stream, ratio} = useVideoStream();
+  const { stream, ratio } = useVideoStream();
   // OpenCV loading state
   const loaded = useLoadCV();
   // Init Ready state
-  const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState(false);
+  const [processing, setProcessing] = useState(false);
 
   // Get camera stream
   useEffect(() => {
@@ -27,7 +32,7 @@ export default function Stream() {
       videoElement.current.srcObject = stream;
   }, [videoElement, stream]);
 
-  // Adjust video and canvas sizes
+  // Adjust video width and canvas sizes
   useEffect(() => {
     // Set width on Canvas and Video elements
     if (window) setWidth(window.innerWidth);
@@ -35,19 +40,52 @@ export default function Stream() {
     if (videoElement !== null && rect !== undefined) setHeight(rect.height);
   }, [videoElement, rect]);
 
-  // set Ready
-  useEffect(()=>{
-    if(ratio && loaded && ratio * height === width) setReady(true);
-  }, [ratio, width, height, loaded, ready])
+  // set ready when openCV loaded
+  useEffect(() => {
+    if (ratio && loaded && ratio * height === width) setReady(true);
+  }, [ratio, width, height, loaded, ready]);
 
   // Trigger OpenCV transformation
   useEffect(() => {
-    if (ready) processCV(videoElement, outputElement);
-  }, [ready, videoElement, outputElement]);
+    console.log(objMode);
+    if (ready && objMode && !processing) {
+      const processingImage = async () => {
+        setProcessing(true);
+        try {
+          await processCV(videoElement, inputElement, outputElement, height, width);
+        } catch (error) {
+          console.error("Cannot transform image", error);
+        } finally {
+          setProcessing(false);
+        }
+      };
+      processingImage();
+    }
+    if(!objMode)
+      outputElement.current?.getContext("2d")?.clearRect(0, 0, width, height);
+  }, [objMode, ready, processing, videoElement, inputElement, height, width]);
+
+  useEffect(() => {
+    const objDom = objButton.current;
+    function handleObjClick() {
+      setObjMode(!objMode);
+    }
+    objDom?.addEventListener("click", handleObjClick);
+    return () => {
+      objDom?.removeEventListener("click", handleObjClick);
+    };
+  }, [objMode]);
 
   return (
     <div>
       <video id="camera" width={width} ref={videoElement} autoPlay={true} />
+      <canvas
+        id="input"
+        ref={inputElement}
+        width={width}
+        height={height}
+        className="hidden"
+      />
       <canvas
         id="output"
         ref={outputElement}
@@ -55,6 +93,9 @@ export default function Stream() {
         height={height}
         className="absolute top-0 left-0"
       />
+      <button ref={objButton} className="flex p-4 border-2 border-solid">
+        Obj
+      </button>
     </div>
   );
 }
--- src/lib/processCV.ts
+++ src/lib/processCV.ts
@@ -3,23 +3,29 @@ import cv from "@/service/cv";
 
 export async function processCV(
   videoElement: RefObject<HTMLVideoElement | null>,
-  outputElement: RefObject<HTMLCanvasElement | null>
+  inputElement: RefObject<HTMLCanvasElement | null>,
+  outputElement: RefObject<HTMLCanvasElement | null>,
+  width: number,
+  height: number
 ) {
-  if (videoElement.current && outputElement.current) {
+  if (videoElement.current && outputElement.current && inputElement.current) {
     // Get context
-    const ctx = outputElement.current.getContext("2d");
-    // Get size params
-    const width = videoElement.current.clientWidth;
-    const height = videoElement.current.clientHeight;
-    if (ctx) {
+    const inputCtx = inputElement.current.getContext("2d", {
+      willReadFrequently: true,
+    });
+    const outputCtx = outputElement.current.getContext("2d", {
+      willReadFrequently: true,
+    });
+    if (inputCtx && outputCtx) {
       // Draw video to canvas
-      ctx.drawImage(videoElement.current, 0, 0, width, height);
+      inputCtx.drawImage(videoElement.current, 0, 0, width, height);
       // Get video stream image
-      const inputImage = ctx.getImageData(0, 0, width, height);
+      const inputImage = inputCtx.getImageData(0, 0, width, height);
       const processedImage = await cv.processVideo(inputImage);
-      console.log(processedImage.data.payload);
-      ctx.putImageData(processedImage.data.payload, 0, 0);
-      //return false;
+      outputCtx.putImageData(processedImage.data.payload, 0, 0);
     }
   }
 }
+
+add edge and contours
+add blur, canny, closing (dilate + erode) and contours on worker
